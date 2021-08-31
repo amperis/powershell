ea79d3e4045434f6973976c920b2edcdfb08ccf4
AMPERIS <38817767+amperis@users.noreply.github.com>
2021-08-31 14:00:14 +0200

    Create change-psw-disabled-users.ps1

---
+++ change-psw-disabled-users.ps1
@@ -0,0 +1,34 @@
+function New-RandomPassword2 {
+    param(
+        [Parameter()]
+        [int]$MinimumPasswordLength = 12,
+        [Parameter()]
+        [int]$MaximumPasswordLength = 13,
+        [Parameter()]
+        [int]$NumberOfAlphaNumericCharacters = 1,
+        [Parameter()]
+        [switch]$ConvertToSecureString
+    )
+    
+    Add-Type -AssemblyName 'System.Web'
+    $length = Get-Random -Minimum $MinimumPasswordLength -Maximum $MaximumPasswordLength
+    $password = [System.Web.Security.Membership]::GeneratePassword($length,$NumberOfAlphaNumericCharacters)
+    if ($ConvertToSecureString.IsPresent) {
+        ConvertTo-SecureString -String $password -AsPlainText -Force
+    } else {
+        $password
+    }
+}
+
+$usuarios = Get-ADUser -Filter {Enabled -eq $false}
+
+$i=0
+foreach($usuario in $usuarios) {
+   $i++
+   $psw = New-RandomPassword2 
+   write-host "$($usuario.SamAccountName) [ $($psw) ]"
+   Set-ADAccountPassword -Identity $usuario.SamAccountName -Reset -NewPassword (ConvertTo-SecureString -AsPlainText "$psw" -Force)
+}
+
+write-host "--------------"
+write
